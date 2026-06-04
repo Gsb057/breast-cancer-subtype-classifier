04c65ab9774edca2332cfa097b47529744736ff2
Sathyabharathi <143860439+Gsb057@users.noreply.github.com>
2026-06-04 22:16:58 +0530

    transpose gene exp file, reorder phenotype file to match gene exp file, split data to train/test. cleaning complete.

--- scripts/01_clean_data.py
+++ scripts/01_clean_data.py
@@ -4,7 +4,6 @@ import pandas
 raw_phenotype_file = pandas.read_csv(r"raw_data\TCGA.BRCA.sampleMap_BRCA_clinicalMatrix", sep='\t')
 subtype_column = raw_phenotype_file['PAM50Call_RNAseq']
 filtered = raw_phenotype_file[subtype_column.isin(['LumA','Basal'])]
-filtered.to_csv('output/cleaned_phenotype_data.csv', index = False)
 
 #match patients id across both files:
 raw_expression_file = pandas.read_csv(r"raw_data\HiSeqV2", sep='\t')
@@ -24,4 +23,25 @@ duplicate_genes = filtered_exp_file.duplicated(subset='sample', keep= False).sum
 print(duplicate_genes)
 '''
 
-filtered_exp_file.to_csv('output/cleaned_expression_data.csv', index=False)
+#transposing the expression file cause machine learning needs it that way.
+transposed_exp_file = filtered_exp_file.set_index('sample').T
+
+#reordering the phenotype file to match the gene expression file.
+filtered = filtered.set_index('sampleID')
+filtered = filtered.reindex(transposed_exp_file.index)
+
+transposed_exp_file.to_csv('output/cleaned_expression_data.csv')
+filtered.to_csv('output/cleaned_phenotype_data.csv')
+
+#splittning train/test data 80/20:
+from sklearn.model_selection import train_test_split
+
+X = transposed_exp_file
+y = filtered['PAM50Call_RNAseq']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 42, stratify= y)
+
+X_train.to_csv('output/train_X.csv')
+X_test.to_csv('output/test_X.csv')
+y_train.to_csv('output/train_y.csv')
+y_test.to_csv('output/test_y.csv')
+
